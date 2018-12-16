8725319b6ec7d9a92b6c96484948df73a07c476b
nadeeraka <nimanthanadeeraka@outlook.com>
2018-12-16 13:34:26 +0530

    style added

--- src/components/result.js
+++ src/components/result.js
@@ -2,7 +2,21 @@ import React from "react";
 
 const Result = props => (
   <div>
-    <img src={props.textValue.previewURL} alt="" />
+    <div className="container">
+      <div className="row">
+        <div className="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12" />
+
+        <br />
+        <div className="left">
+          <div className="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+            <img
+              src={props.textValue.largeImageURL}
+              className="img-responsive"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
